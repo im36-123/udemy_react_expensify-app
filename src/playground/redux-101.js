e83e474e3fdbda7e70e0122f95fe790acbed4118
im36-123 <im36.1230@gmail.com>
2018-10-02 17:48:23 +0900

    add: Action Creator

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,10 +1,55 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
-  console.log("101");
-  return state;
+// Action Creator - functions that return action objects
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  incrementBy
 });
 
-console.log(store.getState());
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy
+});
+
+const setCount = ({ setTo = 0 } = {}) => ({
+  type: "SET",
+  setTo
+});
+
+const reseCount = () => ({
+  type: "RESET"
+});
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      console.log(action);
+      return { count: state.count + action.incrementBy };
+
+    case "DECREMENT":
+      return { count: state.count - action.decrementBy };
+
+    case "SET":
+      return { count: action.setTo };
+
+    case "RESET":
+      return { count: 0 };
+
+    default:
+      return state;
+  }
+});
+
+store.subscribe(() => console.log(store.getState()));
+
+// store.dispatch({
+//   type: "INCREMENT",
+//   incrementBy: 5
+// });
+
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(decrementCount({ decrementBy: 5 }));
+store.dispatch(setCount({ setTo: 100 }));
+store.dispatch(reseCount());
 
 export default store;
