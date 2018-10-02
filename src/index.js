72f9b3fe270ff30f4b9375b464241c0376dc633f
im36-123 <im36.1230@gmail.com>
2018-10-02 20:36:53 +0900

    add: combine reducers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import AppRouter from './routers/AppRouter'
 import "../node_modules/normalize.css/normalize.css";
 import "./styles/style.css";
-import redux from "./playground/redux-101"
+import store from "./playground/redux-expensify"
 
-redux
+store
 
 ReactDOM.render(<AppRouter/>, document.getElementById("root"));
