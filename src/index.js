3e6fd7340c82d289f0002edf572ad8dcfc403507
im36-123 <im36.1230@gmail.com>
2018-10-04 16:03:44 +0900

    update: 役割ごとにファイル分割

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppRouter from './routers/AppRouter'
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "../node_modules/normalize.css/normalize.css";
 import "./styles/style.css";
-import store from "./playground/redux-expensify"
 
-store
+const store = configureStore();
 
-ReactDOM.render(<AppRouter/>, document.getElementById("root"));
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addExpense({ description: "Water bill" }));
+store.dispatch(addExpense({ description: "Gas bill" }));
+
+store.dispatch(setTextFilter("gas"));
+
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters));
+
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
